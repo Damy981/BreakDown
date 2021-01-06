2750bad78781b3c3501bbcdeb8fd0dfb6b5228d6
valeriasantorsola <valeriasantorsola@outlook.it>
2021-01-06 21:51:38 +0100

    Creazione immagine profilo

---
+++ app/src/main/java/com/example/android/arkanoid/Classes/ProfileImageGenerator.java
@@ -0,0 +1,111 @@
+package com.example.android.arkanoid.Classes;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.Shape;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+// This is a ProfileImageGenerator. It is used to retrieve or generate a drawing to use as a profile picture
+
+public class ProfileImageGenerator
+{
+    // Context used to perform the functionality of this object
+    private Context context;
+    // Drawable loaded (o generated)
+    private Drawable resource;
+
+    // Interface used to provide an implementation of the callback method invoked when the resource is ready
+    public interface OnImageGeneratedListener
+    { void onImageGenerated(Drawable drawable); }
+
+    @SuppressLint("CheckResult")
+    public ProfileImageGenerator(@NonNull Context context)
+    {
+        this.context = context;
+    }
+
+    //  Generates an image for the specified name
+    @SuppressLint("CheckResult")
+    public void fetchImageOf(@NonNull String name, OnImageGeneratedListener onImageGeneratedListener)
+    {
+        resource = new ProfileImageLetter(name);
+
+        if(onImageGeneratedListener != null)
+            onImageGeneratedListener.onImageGenerated(resource);
+    }
+
+    public Context getContext()
+    {
+        return context;
+    }
+
+    public Drawable getResource()
+    {
+        return resource;
+    }
+}
+
+// Drawable generated from a given string
+class ProfileImageLetter extends ShapeDrawable
+{
+    // Letter to be displayed in the profile picture
+    private char letter;
+
+    ProfileImageLetter(String name)
+    {
+        if( name != null && ! name.isEmpty()) {
+            this.letter = name.toUpperCase().charAt(0);
+        }
+        setShape(new OvalShape());
+        setIntrinsicWidth(512);
+        setIntrinsicHeight(512);
+        setColorFilter( generateColorFor(name) , PorterDuff.Mode.SRC_ATOP);
+    }
+
+    @Override
+    protected void onDraw(Shape shape, Canvas canvas, Paint paint2)
+    {
+        super.onDraw(shape, canvas, paint2);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setTextSize(350);
+        paint.setTextAlign(Paint.Align.CENTER);
+
+        canvas.drawText( "" + letter , ((float) getBounds().width()/2),
+                ((float) getBounds().height()/2) - ((paint.descent() + paint.ascent()) /2), paint);
+    }
+
+    // Generates a color based on the specified string.
+    private int generateColorFor(String name)
+    {
+        if( name == null || name.isEmpty())
+            return Color.TRANSPARENT;
+
+        int color = Color.rgb(mod(name.hashCode(), 255),
+                mod(name.hashCode() / 255, 255),
+                mod(name.hashCode() / (255 * 255), 255));
+
+        return color;
+    }
+
+    // Calculation of an equivalent number from x modulo y
+    private int mod(int x, int y)
+    {
+        while(x < 0)
+            x += y;
+
+        return x % y;
+    }
+
+}
--- app/src/main/java/com/example/android/arkanoid/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/android/arkanoid/Fragments/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.android.arkanoid.Fragments;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,13 +12,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.arkanoid.Activities.LoginActivity;
 import com.example.android.arkanoid.Classes.Profile;
+import com.example.android.arkanoid.Classes.ProfileImageGenerator;
 import com.example.android.arkanoid.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
 
 
 public class ProfileFragment extends Fragment implements View.OnClickListener {
@@ -27,6 +31,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     private TextView tvUsername;
     private TextView tvCoins;
     private FirebaseAuth mAuth;
+    private ImageView profileImage;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -42,22 +47,36 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         tvCoins = getView().findViewById(R.id.tvCoins);
 
         Profile profile = (Profile) getArguments().getSerializable("profile");
-
+        profileImage = view.findViewById(R.id.imageView_profile);
         tvUsername.setText(profile.getUserName());
         tvCoins.setText(String.valueOf(profile.getCoins()));
 
-        if (user == null){
+        // Profile image generation
+        if (user != null) {
+            new ProfileImageGenerator(getContext())
+                    .fetchImageOf(profile.getUserName(), new ProfileImageGenerator.OnImageGeneratedListener() {
+                        @Override
+                        public void onImageGenerated(Drawable drawable) {
+                            profileImage.setImageDrawable(drawable);
+                        }
+                    });
+
+
+
+        } else {
             guestRegisterButton.setVisibility(View.VISIBLE);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+
 
     @Override
     public void onClick(View view) {
