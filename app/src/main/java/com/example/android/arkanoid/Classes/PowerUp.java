2ef723725932a28c5b74c6ce09ed3d69696fd809
cri <cristinamatino98@gmail.com>
2021-01-07 22:19:09 +0100

    added power up constants

--- app/src/main/java/com/example/android/arkanoid/Classes/PowerUp.java
+++ app/src/main/java/com/example/android/arkanoid/Classes/PowerUp.java
@@ -4,6 +4,13 @@ import java.io.Serializable;
 
 public class PowerUp implements Serializable {
 
+    //int constant which symbolize the power-up position in the Profile.PowerUps arraylist
+    static final int COINS_DROP_RATE = 0;
+    static final int PADDLE_LENGTH = 1;
+    static final int FREEZE = 2;
+    static final int ITEM4 = 3;
+    static final int ITEM5 = 4;
+    //-----------------------------------------
     private String name;
     private int price;
     private int quantity;
