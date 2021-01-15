9ad4b142f0f28412af6cd44a0357589765d5e86b
Damy981 <dpalmisano98@gmail.com>
2021-01-15 22:25:24 +0100

    now ranking orders by score

--- app/src/main/java/com/example/android/arkanoid/Fragments/RankingFragment.java
+++ app/src/main/java/com/example/android/arkanoid/Fragments/RankingFragment.java
@@ -19,8 +19,11 @@ import com.example.android.arkanoid.Classes.Adapters.RankItemAdapter;
 import com.example.android.arkanoid.R;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RankingFragment extends Fragment {
@@ -59,6 +62,8 @@ public class RankingFragment extends Fragment {
         rankingMap = SplashScreenActivity.rankingMap;
 
         if(rankingMap != null) {
+            rankingMap = sortHashMapByValues(rankingMap);
+
             Iterator i = rankingMap.entrySet().iterator();
             while (i.hasNext()) {
                 Map.Entry pair = (Map.Entry) i.next();
@@ -76,5 +81,37 @@ public class RankingFragment extends Fragment {
                     .setPositiveButton(android.R.string.ok, null)
                     .setIcon(android.R.drawable.ic_dialog_alert)
                     .show();
+    }
+
+    private LinkedHashMap<String, String> sortHashMapByValues(
+            HashMap<String, String> passedMap) {
+        List<String> mapKeys = new ArrayList<>(passedMap.keySet());
+        List<String> mapValues = new ArrayList<>(passedMap.values());
+        Collections.sort(mapValues);
+        Collections.sort(mapKeys);
+        Collections.reverse(mapValues);
+        Collections.reverse(mapKeys);
+
+        LinkedHashMap<String, String> sortedMap = new LinkedHashMap<>();
+
+        Iterator<String> valueIt = mapValues.iterator();
+
+        while (valueIt.hasNext()) {
+            String val = String.valueOf(valueIt.next());
+            Iterator<String> keyIt = mapKeys.iterator();
+
+            while (keyIt.hasNext()) {
+                String key = keyIt.next();
+                String comp1 = String.valueOf(passedMap.get(key));
+                String comp2 = val;
+
+                if (comp1.equals(comp2)) {
+                    keyIt.remove();
+                    sortedMap.put(key, val);
+                    break;
+                }
+            }
+        }
+        return sortedMap;
     }
 }
